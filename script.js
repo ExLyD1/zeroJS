188b3fffe01e0d702c8eb1ee2df07a322ed6f43c
ExLyD1 <sashaahnij@gmail.com>
2024-05-01 22:48:01 +0300

    qwe

---
+++ script.js
@@ -0,0 +1,99 @@
+// let nameU = null;
+// let passU = null;
+// let dickSize = null;
+// const userList = {
+//   name: nameU,
+//   password: passU,
+//   dick: dickSize,
+// }
+// console.log(userList.name)
+
+// const people = {
+//   name: 'alex',
+//   age: 22,
+//   dick: 12,
+// }
+// people['ass'] = 11
+
+// // создания нового обьекта с скопированого обьекта
+// const people2 = Object.assign({}, people)
+// const people3 = {...people2}
+
+
+// people.dick = 'suck'
+// people2.age = 26
+// delete people3.dick 
+// delete people3.ass 
+// delete people3.name
+
+
+// console.log(people);
+// console.log(people2);
+// console.log(people3);
+
+// console.log('0000');
+// let a = 21
+// console.dir(a)
+
+// let a = 20
+// let b = 'suckl'
+  
+// function sex() {
+//   a = 'true'
+//   b = 20
+//   b = 'sex'
+  
+// }
+// sex()
+// console.log(a, b);
+
+// let a =0;
+// console.log(!!a);
+
+
+
+// let array = [1,2,3]
+// console.log(array);
+
+// array.push('piska')
+// console.log(array);
+
+// array.push(true)
+// console.log(array);
+
+// const removedArr = array.pop()
+// console.log(array);
+
+// array.unshift('sosat')
+// console.log(array);
+
+// array.shift()
+// console.log(array);
+
+// const newArr = array.map(el => el*2)
+// console.log(newArr);
+
+// array.map(el => el*3)
+// console.log(array);
+
+
+
+// const userProfile = {
+//   name : 'bogdan',
+//   comments: 23,
+//   has: true,
+// }
+
+// const {name, comments} = userProfile // деструктуризация обьектов
+
+// // const userInfo = ({name,comments,has}) - можно и так деструктизировать
+
+// const userInfo = (userProfile) => {
+//   if (!comments) {
+//     console.log(`user ${name} has no comments`)
+// }
+//   console.log(`user ${name} has ${comments} comments`) 
+// }
+// userInfo(userProfile)
+
+
